fb3ff749a70d1eb97698268c27a1bf58adede55a
Swapnaja Achintalwar <swapnaja9860@gmail.com>
2023-08-12 10:02:20 +0530

    add header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Container} from "react-bootstrap"
 
 const App = () => {
   return (
-    <div>Welcome to Proshop</div>
+    <>
+    <Header/>
+    {/* py-3 padding on the y-axis top and bottom */}
+    <main className='py-3'>
+      <Container>
+         <h1>Welcome to Proshop </h1>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navbar, Nav, Container}  from 'react-bootstrap'
+import {FaShoppingCart, FaUser} from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <header>
+        {/* whenever the screen is medium in size or less than that show Hamburger */}
+        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+            <Container>
+                <Navbar.Brand href='/'>ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    {/* ms-auto align links to the right 
+                    me-auto align-links to the left next to ProShop */}
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/cart'><FaShoppingCart/> Cart</Nav.Link>
+                        <Nav.Link href='/login'><FaUser/>Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
