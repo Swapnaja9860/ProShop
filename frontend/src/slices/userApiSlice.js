a59d16c241988228b1e61c9b1000f7112232e0f2
Swapnaja Achintalwar <swapnaja9860@gmail.com>
2023-11-01 09:38:16 +0530

    add authentication and login screen

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,18 @@
+import {USERS_URL} from '../constants';
+import { apiSlice } from './apiSlice';
+
+// we can use the api slicing instead of fetch and axios
+export const userApiSlice = apiSlice.injectEndpoints({
+    endpoints : (builder) => ({
+        // for GET method --> builder.query and POST metod --> builder.mutation
+        login: builder.mutation({
+            query: (data) => ({
+                url : `${USERS_URL}/auth`,
+                method: 'POST',
+                body : data,
+            }),
+        })
+    })
+})
+
+export const {useLoginMutation} = userApiSlice;
